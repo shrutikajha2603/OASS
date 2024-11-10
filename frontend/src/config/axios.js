aeb0b37a879814747922843d5db4227bb2a008b6
Shreyam Kundu <shreyamkundu04@gmail.com>
2024-11-10 19:43:53 +0530

    backend url changed

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,3 +1,4 @@
 import axios from 'axios'
+const REACT_APP_BASE_URL="http://localhost:8000" ;
 
-export const axiosi=axios.create({withCredentials:true,baseURL:process.env.REACT_APP_BASE_URL})
+export const axiosi=axios.create({withCredentials:true,baseURL:REACT_APP_BASE_URL})
